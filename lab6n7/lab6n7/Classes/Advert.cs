fe9d22127bd72a70eb947f106eb95fef3440be94
Alexei <krisfragerfeed@gmail.com>
2021-03-25 12:09:14 +0300

    feat: update lab6/7

--- lab6n7/lab6n7/Classes/Advert.cs
+++ lab6n7/lab6n7/Classes/Advert.cs
@@ -1,19 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace lab6n7
 {
     public class Advert
     {
-        public int ID { get; set; } // Идентификатор объявления
-        public string FullName { get; set; } // Полное название товара REGEXP: ^(?i)[a-zа-я0-9-_\s]{3,32}$
-        public string ShortName { get; set; } // Краткое название товара REGEXP: ^(?i)[a-zа-я0-9-_\s]{3,16}$
-        public string Category { get; set; } // Категория товара
-        public double Raiting { get; set; } // Рейтинг товара REGEXP: ^(\d{1},\d{1})$|^(\d{1}|1[0])$
-        public decimal Cost { get; set; } // Цена товара REGEXP: ^(\d)$|^(\d*[,]\d{1,2})$
-        public int Amount { get; set; } // Количество товара на складе REGEXP: ^(\d{1,20})$
+        public int id;
+        public string fullName;
+        public string shortName;
+        public string category;
+        public double raiting;
+        public decimal cost;
+        public int amount;
+        public List<Picture> images;
+
+        public int ID // Идентификатор объявления
+        { 
+            get { return id; } 
+            set
+            {
+                id = value;
+                OnPropertyChanged("ID");
+            } 
+        } 
+        public string FullName // Полное название товара
+        {
+            get { return fullName; }
+            set
+            {
+                fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        } 
+        public string ShortName // Краткое название товара
+        {
+            get { return shortName; }
+            set
+            {
+                shortName = value;
+                OnPropertyChanged("ShortName");
+            }
+        } 
+        public string Category // Категория товара
+        {
+            get { return category; }
+            set
+            {
+                category = value;
+                OnPropertyChanged("Category");
+            }
+        } 
+        public double Raiting // Рейтинг товара
+        {
+            get { return raiting; }
+            set
+            {
+                raiting = value;
+                OnPropertyChanged("Raiting");
+            }
+        } 
+        public decimal Cost // Цена товара
+        {
+            get { return cost; }
+            set
+            {
+                cost = value;
+                OnPropertyChanged("Cost");
+            }
+        } 
+        public int Amount // Количество товара на складе
+        {
+            get { return amount; }
+            set
+            {
+                amount = value;
+                OnPropertyChanged("Amount");
+            }
+        } 
         public List<Picture> Images { get; set; } = new List<Picture>(); // Картинки к объявлению
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
 
         public int GetID()
         {
---
+++ lab6n7/lab6n7/Classes/ApplicationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6n7.Classes
+{
+    class ApplicationViewModel
+    {
+        private Advert inputAdvert;
+
+        public List<Advert> Adverts { get; set; }
+        public Advert InputAdvert
+        {
+            get { return inputAdvert; }
+            set
+            {
+                inputAdvert = value;
+                OnPropertyChanged("InputAdvert");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Adverts = Serialization.Deserialize();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
