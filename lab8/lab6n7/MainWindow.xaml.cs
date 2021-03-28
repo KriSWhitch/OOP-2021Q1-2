95784e4daa92c5f6fd8ed9bfb6489fa0595fc32d
Alexei <krisfragerfeed@gmail.com>
2021-03-28 16:09:21 +0300

    feat: add lab8

---
+++ lab8/lab6n7/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+
+namespace lab6n7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Mouse.OverrideCursor = ((FrameworkElement)this.Resources["MouseCursor"]).Cursor;
+            App.LanguageChanged += LanguageChanged;
+
+            CultureInfo currLang = App.Language;
+
+            //Заполняем меню смены языка:
+            ComboBoxLanguages.Items.Clear();
+            foreach (var lang in App.Languages)
+            {
+                ComboBoxItem menuLang = new ComboBoxItem();
+                menuLang.Content = lang.DisplayName;
+                menuLang.Tag = lang;
+                menuLang.IsSelected = lang.Equals(currLang);
+                menuLang.Selected += ChangeLanguageClick;
+                ComboBoxLanguages.Items.Add(menuLang);
+            }
+            ComboBoxLanguages.SelectedIndex = 1;
+            // Заполняем меню выбора темы
+            List<string> styles = new List<string> { "StandartTheme","LightTheme", "DarkTheme" };
+            ComboBoxThemes.SelectionChanged += ThemeChange;
+            ComboBoxThemes.ItemsSource = styles;
+            ComboBoxThemes.SelectedItem = "StandartTheme";
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = ComboBoxThemes.SelectedItem as string;
+            // определяем путь к файлу ресурсов
+            var uri = new Uri("Resources/" +style + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+        private void LanguageChanged(object sender, EventArgs e)
+        {
+            CultureInfo currLang = App.Language;
+
+            //Отмечаем нужный пункт смены языка как выбранный язык
+            foreach (ComboBoxItem i in ComboBoxLanguages.Items)
+            {
+                CultureInfo ci = i.Tag as CultureInfo;
+                i.IsSelected = ci != null && ci.Equals(currLang);
+            }
+        }
+
+        private void ChangeLanguageClick(object sender, EventArgs e)
+        {
+            ComboBoxItem mi = sender as ComboBoxItem;
+            if (mi != null)
+            {
+                CultureInfo lang = mi.Tag as CultureInfo;
+                if (lang != null)
+                {
+                    App.Language = lang;
+                }
+            }
+        }
+
+        private void ButtonPopUpLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void GitHubButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/KriSWhitch/OOP-2021Q1-2/tree/master/lab6n7");
+        }
+
+        private void ButtonFAQ_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Version 1.0 ©KriSWhitch", "FAQ", MessageBoxButton.OK);
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Children.Clear();
+            GridMain.Children.Add(new HomePage(this));
+        }
+
+        private void CreateAdButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Children.Clear();
+            GridMain.Children.Add(new CreateAdPage());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HomeButton_Click(sender, e);
+        }
+    }
+}
---
+++ lab8/lab6n7/Pages/HomePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab6n7
+{
+    /// <summary>
+    /// Логика взаимодействия для HomePage.xaml
+    /// </summary>
+    public partial class HomePage : UserControl
+    {
+        MainWindow mainForm;
+        public List<AdvertInGrid> ResultCollection { get; set; }
+
+        public HomePage(MainWindow mainForm)
+        {
+            this.mainForm = mainForm;
+            InitializeComponent();
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Advert> adList = Serialization.Deserialize();
+            ResultCollection = new List<AdvertInGrid>();
+            foreach (var el in adList)
+            {
+                ResultCollection.Add(new AdvertInGrid { FullName = el.FullName, ShortName = el.ShortName, Category = el.Category, Raiting = el.Raiting, Cost = el.Cost, Amount = el.Amount, AdvertImage = ImageConverter.ImageSourceFromBitmap(el.Images[0].Source), Images = el.Images, ID = el.ID });
+            }
+            MainGrid.ItemsSource = ResultCollection;
+        }
+
+        private void RowDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            mainForm.GridMain.Children.Clear();
+            mainForm.GridMain.Children.Add(new AdvertPage(sender, e, mainForm));
+        }
+    }
+}
